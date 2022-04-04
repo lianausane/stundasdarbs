6cdd1c5b4450ea1ad497e11d06ecccd4ae0a7005
katriiinaak <102228708+katriiinaak@users.noreply.github.com>
2022-04-04 15:31:40 +0300

    hnui

---
+++ kopaa.py
@@ -0,0 +1,5 @@
+x = int(input("Izvēlies vienu no figūrām, kuru aprēķināsi: riņķis-1 , trijstūris-2 , četrstūris-3"))
+if x==1:
+    y = int(input("Izvēlies, ko aprēķināsi; perimetrs-1 , laukums-2 , diametrs-3"))
+    if y==1:
+        a = int(input("Ievadi rādiusu:"))
