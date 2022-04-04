f421eac5350f60ef078ca1f7a6d28c528c89f013
lianausane <102228831+lianausane@users.noreply.github.com>
2022-04-04 15:50:15 +0300

    yxvd

--- kopaa.py
+++ kopaa.py
@@ -1,5 +1,9 @@
+import liana
+
 x = int(input("Izvēlies vienu no figūrām, kuru aprēķināsi: riņķis-1 , trijstūris-2 , četrstūris-3"))
 if x==1:
     y = int(input("Izvēlies, ko aprēķināsi; perimetrs-1 , laukums-2 , diametrs-3"))
-    if y==1:
+    if y==1:1
+
         a = int(input("Ievadi rādiusu:"))
+        print("Laukums ir", liana.rinka_lauk(a))
---
+++ liana.py
@@ -0,0 +1,5 @@
+import math
+
+def rinka_lauk(r):
+    s = math.pi * r**2
+    return s
